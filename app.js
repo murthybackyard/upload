251c145dcaec1087481cc0bcaff9715aa72ee055
murthybackyard <murthy.patnaik@gmail.com>
2016-10-17 16:10:42 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,52 +1,20 @@
 var express = require('express');
 var app = express();
-var path = require('path');
-var formidable = require('formidable');
-var fs = require('fs');
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.set('port', (process.env.PORT || 5000));
 
-//app.get('/', function(req, res){
-//  res.sendFile(path.join(__dirname, 'views/index.html'));
-//});
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
 app.set('views', __dirname + '/views');
+//app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('index.html');
+  response.render('index');
 });
 
-app.post('/upload', function(req, res){
-
-  // create an incoming form object
-  var form = new formidable.IncomingForm();
-
-  // specify that we want to allow the user to upload multiple files in a single request
-  form.multiples = true;
-
-  // store all uploads in the /uploads directory
-  form.uploadDir = path.join(__dirname, '/uploads');
-
-  // every time a file has been uploaded successfully,
-  // rename it to it's orignal name
-  form.on('file', function(field, file) {
-    fs.rename(file.path, path.join(form.uploadDir, file.name));
-  });
-
-  // log any errors that occur
-  form.on('error', function(err) {
-    console.log('An error has occured: \n' + err);
-  });
-
-  // once all the files have been uploaded, send a response to the client
-  form.on('end', function() {
-    res.end('success');
-  });
-
-  // parse the incoming request containing the form data
-  form.parse(req);
-
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
 
-var server = app.listen(process.env.PORT || 3000, function(){
-  console.log('Server listening on port ',http.address().port);
-});
+
