78ddac9ca761208ec404b8f91f6519a647234649
murthybackyard <murthy.patnaik@gmail.com>
2016-10-17 00:36:20 -0700

    added log stmt for port

--- app.js
+++ app.js
@@ -43,5 +43,5 @@ app.post('/upload', function(req, res){
 });
 
 var server = app.listen(process.env.PORT || 3000, function(){
-  console.log('Server listening on port 3000');
+  console.log('Server listening on port ',http.address().port);
 });
